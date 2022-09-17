c273081b44b8f964eabd6c6c70c6af5a8cd8c0eb
Kronk21 <CesarAxel001@gmail.com>
2022-09-17 12:30:07 -0500

    Frontend del formulario creado y listo para reutilizarse

---
+++ includes/formulario.php
@@ -0,0 +1,18 @@
+<section class="seccion-formulario">
+    <div class="contenedor">
+        <div class="formulario__contenido">
+            <h2>Envianos un mensaje</h2>
+            <form action="#" class="formulario" method="">
+                <div class="formulario__control">
+                    <label for="correo">Tu correo electronico:</label>
+                    <input type="email" name="correo" id="correo">
+                </div>
+                <div class="formulario__control">
+                    <label for="mensaje">Escribe un mensaje:</label>
+                    <textarea name="mensaje" id="mensaje"></textarea>
+                </div>
+                <input type="submit" name="enviar" class="btn btn-secundario" value="Enviar">
+            </form>
+        </div>
+    </div>
+</section>
