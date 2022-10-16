7e471ae6f9e9cdc3eb390d709e6eb0c7eb445435
Kronk21 <CesarAxel001@gmail.com>
2022-10-15 20:51:44 -0500

    Menu de administrador mas completo, se pueden actualizar productos

---
+++ admin/index.php
@@ -0,0 +1,61 @@
+<?php
+    include "../includes/conexion.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/a906dbdcb6.js" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" href="../src/css/utilidades.css">
+    <link rel="stylesheet" href="../src/css/estilos.css">
+    <link rel="stylesheet" href="../src/css/queries.css">    
+    <link rel="stylesheet" href="../src/css/admin.css">    
+
+    <script defer src="../src/js/script.js"></script>
+    
+    <title>Administrador</title>
+</head>
+<body>
+    <section class="seccion-admin">
+        <div class="contenedor">
+            <h1>Panel de administrador</h1>
+
+            <div class="hero__botones">
+                <a href="crear_producto.php" class="btn btn-primario">Crear producto</a>
+                <a href="cerrar_sesion.php" class="btn btn-primario">Cerrar sesión</a>
+            </div>
+
+            <div class="paquetes__secundarios">
+                <?php
+                    $query = "SELECT id, nombre FROM productos";
+                    $statement = $conexion->prepare($query);
+                    $statement->execute();
+                    $productos = $statement->get_result();
+
+                    while($producto = $productos->fetch_assoc()):
+                ?> 
+                    <div class="paquete__secundario">
+                        <p class="secundario__nombre">
+                            <?php echo $producto["id"]; ?>.- <?php echo $producto["nombre"]; ?>
+                        </p>
+                        <div class="secundario__linea"></div>
+                        <div class="admin__links">
+                            <a href="update_producto.php?producto_id=<?php echo $producto["id"]; ?>" class="secundario__link">Editar</a>
+                            <a target="_blank" href="../producto.php?producto_id=<?php echo $producto["id"]; ?>" class="secundario__link">Ver</a>                            
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
--- admin/update_producto.php
+++ admin/update_producto.php
@@ -1,28 +1,65 @@
 <?php 
     include "../includes/conexion.php";
 
+    if(!isset($_GET["producto_id"])) {
+        header("Location: index.php");
+    }
+    
+    //  Obtener la informacion del producto
+    $query = "SELECT * FROM productos WHERE id = ?";
+    $statement = $conexion->prepare($query);
+    $statement->execute([$_GET["producto_id"]]);
+    $producto = $statement->get_result()->fetch_assoc();
+
+    if(is_null($producto)) {
+        header("Location: index.php");
+    }
+
     $errores = "";
     $exito = "";
     
-    if(isset($_POST["enviar"])) {
+    if(isset($_POST["update"])) {
         // Almacenar informacion en variables
         $nombre = $_POST["nombre"];
         $precio = $_POST["precio"];
         $descripcion = $_POST["descripcion"];
         $caracteristicas = $_POST["caracteristicas"];
         $cantidad = $_POST["inventario"];
-        $img1 = $_FILES["img1"]["name"];
-        $img2 = $_FILES["img2"]["name"];
-        $img3 = $_FILES["img3"]["name"];
-        $img4 = $_FILES["img4"]["name"];
-        $categoria = $_POST["categoria"];
+        $imagenes = [];
+        
+        $img1 = $_FILES["img1"];
+        if(empty($img1["name"])) {
+            $img1 = $_POST["img1-guardada"];
+        } else {
+            array_push($imagenes, $img1);
+            $img1 = $_FILES["img1"]["name"];
+        }
 
-        $imagenes = [
-            $_FILES["img1"],
-            $_FILES["img2"],
-            $_FILES["img3"],
-            $_FILES["img4"]
-        ];
+        $img2 = $_FILES["img2"];
+        if(empty($img2["name"])) {
+            $img2 = $_POST["img2-guardada"];
+        } else {
+            array_push($imagenes, $img2);
+            $img2 = $_FILES["img2"]["name"];
+        }
+
+        $img3 = $_FILES["img3"];
+        if(empty($img3["name"])) {
+            $img3 = $_POST["img3-guardada"];
+        } else {
+            array_push($imagenes, $img3);
+            $img3 = $_FILES["img3"]["name"];
+        }
+
+        $img4 = $_FILES["img4"];
+        if(empty($img4["name"])) {
+            $img4 = $_POST["img4-guardada"];
+        } else {
+            array_push($imagenes, $img4);
+            $img4 = $_FILES["img4"]["name"];
+        }
+
+        $categoria = $_POST["categoria"];
 
         // Mover las imagenes a la carpeta src/img/db_productos
         $directorio = "../src/img/db_productos/";
@@ -40,14 +77,14 @@
                 move_uploaded_file($img["tmp_name"], $directorio . $img["name"]);
             }
 
-            // Insertar en la base de datos
-            $query = "INSERT INTO productos (nombre, precio, descripcion, caracteristicas, cantidad, ";
-            $query .= "imagen1, imagen2, imagen3, imagen4, categoria_id) ";
-            $query .= "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+            // Actualizar la base de datos
+            $query = "UPDATE productos SET nombre = ?, precio = ?, descripcion = ?, caracteristicas = ?, cantidad = ?, ";
+            $query .= "imagen1 = ?, imagen2 = ?, imagen3 = ?, imagen4 = ?, categoria_id = ? ";
+            $query .= "WHERE id = ?";
             $statement = $conexion->prepare($query);
-            $statement->execute([$nombre, $precio, $descripcion, $caracteristicas, $cantidad, $img1, $img2, $img3, $img4, $categoria]);
+            $statement->execute([$nombre, $precio, $descripcion, $caracteristicas, $cantidad, $img1, $img2, $img3, $img4, $categoria, $_GET["producto_id"]]);
 
-            $exito = "Se agrego el producto a la base de datos";
+            $exito = "Se actualizo el producto de manera exitosa";
         }
     }    
 
@@ -77,7 +114,7 @@
 
     <script defer src="../src/js/script.js"></script>
     
-    <title>Crear productos</title>
+    <title>Actualizar producto</title>
 </head>
 
 <body>    
@@ -92,34 +129,38 @@
                     <div class="formulario__control">
                         <label for="nombre">Nombre del producto:</label>
                         <!-- <input type="text" name="nombre" id="nombre"> -->
-                        <textarea name="nombre" id="nombre"></textarea>
+                        <textarea name="nombre" id="nombre"><?php echo $producto["nombre"]; ?></textarea>
                     </div>
 
                     <div class="formulario__control">
                         <label for="precio">Precio:</label>
-                        <input type="number" name="precio" id="precio">
+                        <input type="number" name="precio" id="precio" value="<?php echo $producto["precio"]; ?>">
                     </div>
 
                     <div class="formulario__control">
                         <label for="descripcion">Descripcion:</label>
-                        <textarea name="descripcion" id="descripcion"></textarea>
+                        <textarea name="descripcion" id="descripcion"><?php echo $producto["descripcion"]; ?></textarea>
                     </div>
 
                     <div class="formulario__control">
                         <label for="caracteristicas">Caracteristicas:</label>
-                        <textarea name="caracteristicas" id="caracteristicas"></textarea>
+                        <textarea name="caracteristicas" id="caracteristicas"><?php echo $producto["caracteristicas"]; ?></textarea>
                     </div>
 
                     <div class="formulario__control">
                         <label for="inventario">Cantidad:</label>
-                        <input type="number" name="inventario" id="inventario">
+                        <input type="number" name="inventario" id="inventario" value="<?php echo $producto["cantidad"]; ?>">
                     </div>
 
                     <div class="formulario__control">
                         <label for="categoria">Categoria:</label>
                         <select name="categoria" id="categoria">
                             <?php while($fila = $categorias->fetch_assoc()):?>
-                                <option value="<?php echo $fila["id"]; ?>"><?php echo $fila["nombre"]; ?></option>
+                                <?php if($producto["categoria_id"] == $fila["id"]): ?>
+                                    <option selected value="<?php echo $fila["id"]; ?>"><?php echo $fila["nombre"]; ?></option>
+                                <?php else: ?>
+                                    <option value="<?php echo $fila["id"]; ?>"><?php echo $fila["nombre"]; ?></option>
+                                <?php endif; ?>
                             <?php endwhile; ?>
                         </select>
                     </div>
@@ -127,12 +168,19 @@
                     <div class="formulario__control">
                         <label for="img">Imagenes:</label>
                         <input type="file" name="img1" id="img1">
+                        <input type="hidden" name="img1-guardada" value="<?php echo $producto["imagen1"]; ?>" id="img1">
+
                         <input type="file" name="img2" id="img2">
+                        <input type="hidden" name="img2-guardada" value="<?php echo $producto["imagen2"]; ?>" id="img2">
+
                         <input type="file" name="img3" id="img3">
+                        <input type="hidden" name="img3-guardada" value="<?php echo $producto["imagen3"]; ?>" id="img3">
+                        
                         <input type="file" name="img4" id="img4">
+                        <input type="hidden" name="img4-guardada" value="<?php echo $producto["imagen4"]; ?>" id="img4">
                     </div>
 
-                    <input type="submit" name="enviar" class="btn btn-secundario" value="Enviar">
+                    <input type="submit" name="update" class="btn btn-secundario" value="Enviar">
                 </form>
 
                 <p>
